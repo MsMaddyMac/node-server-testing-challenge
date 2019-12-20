87adff7a0864e60b76eafff048ddec630c4830e7
Maddy <mmcintosh2k14@gmail.com>
2019-12-19 19:01:15 -0500

    creates second test for GET endpoint

--- hobbits/hobbits-router.spec.js
+++ hobbits/hobbits-router.spec.js
@@ -14,8 +14,16 @@ const server = require('../api/server');
 // });
 
 describe('GET /hobbits', () => {
-	it('should return data in json format', async () => {
-		const response = await request(server).get('/api/hobbits');
+	it('should return data in JSON format', async () => {
+    const response = await request(server).get('/api/hobbits');
 		expect(response.type).toMatch(/json/i);
-	});
+  });
+  
+  it('should return a 200 OK', function() {
+    return request(server)
+      .get('/api/hobbits')
+      .then(res => {
+        expect(res.status).toBe(200);
+      })
+  })
 });
